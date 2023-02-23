a34b43ed8fe60aff7074370c4cde9acf932493a6
unknown <jacobkorede5@gmail.com>
2023-02-23 20:47:47 +0100

    working on services

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -6,7 +6,17 @@ import monitor from "../../assets/pattern (3).png"
 import check from "../../assets/pattern (1).png"
 import bullseye from "../../assets/pattern.png"
 import star from "../../assets/star.png"
+import mobilephone from "../../assets/services-images/mobilephone.png"
+import Startup from "../../assets/services-images/Startup.png"
+import Megaphone from "../../assets/services-images/Megaphone.png"
+import marketing from "../../assets/services-images/Marketing Plan.png"
+import funnel from "../../assets/services-images/funnel.png"
+import board from "../../assets/services-images/board.png"
+import settings from "../../assets/services-images/settings.png"
+import blue from '../../assets/services-images/vector (1).png'
+
 import "./home.css"
+import ServicesGrid from "../../components/services-component/services-grid"
 
 function Home(){
     return(
@@ -39,7 +49,30 @@ function Home(){
                 <div className="banner-circle"></div>
             </div>
             <div className="home-services">
-                <ServicesContainer/>
+                <div className="services-heading">
+                    <div className="services-title">
+                        <img src={settings} loading='lazy'/>
+                        <div className="title">
+                            <p className="title-sub"><span>// </span>01. SERVICES</p>
+                            <h2 className="title-text">High-impact digital services to take your business to the next level</h2>
+                        </div>
+                        <div>
+                            <Link to="#"> 
+                                <div>Read More</div>
+                                <img src={blue}/>
+                            </Link>
+                        </div>
+                    </div>
+
+                </div>
+                <div className="services-content">
+                    <ServicesGrid image={mobilephone} sub='Social Media Marketing' text='Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum eligendi aliquid doloribus?'/>
+                    <ServicesGrid image={Megaphone} sub='Paid Advertising' text='Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum eligendi aliquid doloribus?'/>
+                    <ServicesGrid image={board} sub='Advanced Analytics' text='Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum eligendi aliquid doloribus?'/>
+                    <ServicesGrid image={Startup} sub='SEO Optimization' text='Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum eligendi aliquid doloribus?'/>
+                    <ServicesGrid image={funnel} sub='Funnel Optimization' text='Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum eligendi aliquid doloribus?'/>
+                    <ServicesGrid image={marketing} sub='Content Marketing' text='Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum eligendi aliquid doloribus?'/>
+                </div>
             </div>
         </div>
     )
