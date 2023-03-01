50e4e8138e2d254fb3dee9bcef1fcdcc5e8676ca
unknown <jacobkorede5@gmail.com>
2023-03-01 01:52:54 +0100

    working

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -6,7 +6,13 @@ import monitor from "../../assets/pattern (3).png"
 import check from "../../assets/pattern (1).png"
 import bullseye from "../../assets/pattern.png"
 import star from "../../assets/star.png"
+import phoneabout from "../../assets/phoneaboutus.png"
+import atsymbol from "../../assets/atsymbol.png"
+import abouticon from "../../assets/abouticon.png"
+import abouticon2 from "../../assets/abouticon2.png"
+
 import "./home.css"
+
 
 import ServicesContainer from "../../components/services-component/services-container";
 
@@ -41,6 +47,46 @@ function Home(){
                 <div className="banner-circle"></div>
             </div>
             <ServicesContainer/>
+            <div className="home-about">
+                <div className="about-image-container">
+                    <img className="about-image" src={phoneabout} loading="lazy"/>
+                </div>
+                <div className="about-container">
+                    <div className="about-sub">
+                        <img src={atsymbol} className="at-symbol" loading="lazy"/>
+                        <div className="about-title">
+                            <p className="about-info"> <span>// </span> 02. ABOUT US</p>
+                            <h2>The #1  number digital services company</h2>
+                        </div>
+                    </div>
+                    <div style={{width:'100%', border:'1px solid #cfcfcf'}}></div>
+                    <p className="about-text"> Lorem ipsum dolor si
+                        t amet consectetur adipisicing elit. Facere vel iure natus veritatis doloribus vero quae fugiat odio repellendus, omnis consequatur id sit nihil 
+                        accusantium! Cum odit asperiores beatae voluptates 
+                        quaerat consectetur? Ullam, iure.
+                    </p>
+                    <div className="about-point">
+                        <div className="points">
+                            <img src={abouticon2 }/>
+                            <div className="point-container">
+                                <h3 className="point-header">Guaranteed Results</h3>
+                                <div className="linebreaker"/>
+                                <p className="point-text">Lorem ipsum dolor, sit amet consectetur 
+                                adipisicing elit. Et eveniet nihil non? Adipisci provident debitis dicta illo.</p>
+                            </div>
+                        </div>
+                        <div className="points" style={{marginTop:'25px'}}>    
+                            <img src={abouticon}/>
+                            <div className="point-container">
+                                <h3 className="point-header">Team Of Industry Experts</h3>
+                                <div className="linebreaker"/>
+                                <p className="point-text">Lorem ipsum dolor, sit amet consectetur 
+                                adipisicing elit. Et eveniet nihil non? Adipisci provident debitis dicta illo.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>  
+            </div>
         </div>
     )
 }
