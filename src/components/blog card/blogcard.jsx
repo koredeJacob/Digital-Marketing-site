8daa62d0222620ec34fcfc50cc0bf3367ff8c21d
unknown <jacobkorede5@gmail.com>
2023-04-07 23:47:35 +0100

    finished blog card

--- src/components/blog card/blogcard.jsx
+++ src/components/blog card/blogcard.jsx
@@ -1,20 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import social2 from "../../assets/blogimages/socials 2.png"
 import "./blogcard.css"
 
-const BlogCard = ({image ,buttontext,date,title}) =>{
+const BlogCard = ({image ,buttontext,title}) =>{
     return (
         <div className="blog-card">
             <div className="blog-card-image">
-                <img src={image}/>
+                <img src={image  || social2}/>
             </div>
             <div className="blog-card-meta">
-                <Link>{buttontext}</Link>
-                <p>{date}</p>
+                <Link>{buttontext || "content"}</Link>
+                <p>MARCH 13, 2023</p>
             </div>
             <div className="blog-card-title">
-                <h3>{title}</h3>
+                <h3>{title || "The Best 4 Types Of Marketing Strategies For Small Businesses"}</h3>
             </div>
         </div>
     )
--- src/pages/blog/blog.jsx
+++ src/pages/blog/blog.jsx
@@ -1,15 +1,23 @@
 import React from "react"
 
 import file from "../../assets/blogimages/icon (1).png"
-import articleimage from "../../assets/homeimages/small businesses.png"
+import socials from "../../assets/blogimages/socials 2.png"
+import socials2 from "../../assets/blogimages/socials.png"
+import boxes from "../../assets/blogimages/boxes.png"
+import brain from "../../assets/blogimages/brain.png"
 import Nav from "../../components/navigation/navigation"
 import Banner from "../../components/banner/banner"
 import Header from "../../components/header/header"
 import Article from "../../components/article/article"
 import Subscribe from "../../components/subscribe/subscribe"
 import BlogCard from "../../components/blog card/blogcard"
+import Footer from "../../components/footer/footer"
 import "./blog.css"
 
+const blogs=[{image:socials, title:"The Best 4 Types Of Marketing Strategies For Small Businesses", filter:"content"},
+        {image:brain, title:"Learn How To Increase Trafic To Your Website Organically",filter:"content"},
+        {image:boxes,title:"Five Ways For Agencies To Get Out Of The Recession Alive",filter:"growth"},
+        {image:socials2,title:"How To Use Your Content Marketing Strategy To Improve Retention",filter:"social"}]
 function Blog(){
     return (
         <div>   
@@ -21,12 +29,26 @@ function Blog(){
             </header>
             <div className="blog-section">
                 <Header image={file} light={false} spantext="01 . ARTICLES" content="Browse our content on digital marketing growth"/>
-                <Article image={articleimage} date="MARCH 10, 2023" title="The best 4 types of Marketing Strategies For Small Businesses" background="#FFF3CA" reverse={true}/>
+                <Article image={socials2} date="MARCH 10, 2023" title="The best 4 types of Marketing Strategies For Small Businesses" background="#FFF3CA" reverse={false}/>
             </div>
             <Subscribe/>
-            <div className="blog-cards-container">
+            <div className="blog-container">
+                <div className="blog-controls">
+                    <div className="latest-posts">
+                          <h2>Latest Posts</h2>
+                    </div>                 
+                    <div className="blog-controls-button">
 
+                    </div>
+                </div>
+                <div className="blog-card-container">
+                    <BlogCard/>
+                    <BlogCard/>
+                    <BlogCard/>
+                    <BlogCard/>
+                </div>
             </div>
+            <Footer/>
         </div>
     )
 }
