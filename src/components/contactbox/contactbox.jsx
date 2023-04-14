38f284e601d945d994b8b159df8270dde8678659
unknown <jacobkorede5@gmail.com>
2023-04-14 04:59:04 +0100

    one last commit

--- src/components/contactbox/contactbox.jsx
+++ src/components/contactbox/contactbox.jsx
@@ -1,18 +1,21 @@
 import React from "react"
+import { Link } from "react-router-dom"
 
-const ContactBox = () =>{
+import "./contactbox.css"
+
+const ContactBox = ( {buttontext,image,title,text} ) =>{
     return (
-        <div>
-            <div>
-
-            </div>
-            <div>
-                <div>
-                    <img/>
+        <div className="contact-card">
+            <div className="contact-card-inner"> 
+                <div className="contact-card-button">
+                    <Link to="#">{buttontext}</Link>
                 </div>
-                <div>
-                    <h3></h3>
-                    <p></p>
+                <div className="contact-card-lower">
+                    <img src={image}/>
+                    <div className="contact-card-text">
+                        <h3>{title}</h3>
+                        <p>{text}</p>
+                    </div>
                 </div>
             </div>
         </div>
--- src/pages/Contactus/contactus.jsx
+++ src/pages/Contactus/contactus.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 
 import handshake from "../../assets/aboutimages/handshake.png"
+import phone from "../../assets/aboutimages/phone 2.png"
+import location from "../../assets/aboutimages/location.png";
+import envelope from "../../assets/aboutimages/envelope 2.png"
 import Nav from "../../components/navigation/navigation";
 import Banner from "../../components/banner/banner";
 import Header from "../../components/header/header";
+import ContactBox from "../../components/contactbox/contactbox";
+import Subscribe from "../../components/subscribe/subscribe"
+import Footer from "../../components/footer/footer"
+import "./contactus.css"
 
 function ContactUs(){
     return (
@@ -15,13 +22,52 @@ function ContactUs(){
                 </div>
             </header>
             <div className="contactus-contact-container">
-                <div>
+                <div className="contactus-contact-header">
                     <Header image={handshake} spantext="01 . CONTACT US" content="Let's market awesome together" light={false}/>
                 </div>
-                <div>
-
+                <div className="contactus-cards">
+                    <ContactBox image={envelope} buttontext="Contact" title="Email" text="nextlevel@marketing.com"/>
+                    <ContactBox image={phone} buttontext="Call" title="Phone" text="+1 123 456 789"/>
+                    <ContactBox image={location} buttontext="Office" title="Location" text="10 Sunhouse Street, New York, USA."/>
                 </div>
             </div>
+            <div className="contactus-form">
+                <div className="contactus-form-inner">
+                    <form onSubmit={(e)=>e.preventDefault()} className="contactus-contact-form">
+                        <div className="contactus-formsection">
+                            <div className="formsection-child">
+                                <label htmlFor="name">Full Name *</label>
+                                <input type="text" placeholder="Your Name" name="name"/>
+                            </div>
+                            <div className="formsection-child">
+                                <label htmlFor="email">Your Email *</label>
+                                <input type="email" placeholder="example@youreamail.com" name="email"/>
+                            </div>
+                        </div>
+                        <div className="contactus-formsection">
+                            <div className="formsection-child">
+                                <label htmlFor="company">company *</label>
+                                <input type="text" placeholder="Your Company Name" name="company"/>
+                            </div>
+                            <div className="formsection-child">
+                                <label htmlFor="subject">Subject *</label>
+                                <input type="text" placeholder="How Can We Help" name="subject"/>
+                            </div>
+                        </div>
+                        <div className="contactus-form-message">
+                            <div className="form-message">
+                                <label htmlFor="message">Message *</label>
+                                <textarea placeholder="Hello there,I would like to talk about how to..." maxLength={5000}/>
+                            </div>
+                        </div>
+                        <div className="contactus-form-button">
+                            <input type="submit" value="Send Message" />
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <Subscribe/>
+            <Footer/>
         </div>
     )
 }
